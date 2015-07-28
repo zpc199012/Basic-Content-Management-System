1e0d5b2ff7963f5e77108869694e651c2a7b313e
John Zhao <pengcheng199012zhao@gmail.com>
2015-07-28 17:56:43 -0400

    Add admin.php

--- visitpage/admin.php
+++ visitpage/admin.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
+    <head>
 		<title>admin</title>
+		<link href="CSS/visitpage.css" media="all" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
-
-	</body>
+    <div id="header">
+      <h1>Shanghai Tour</h1>
+    </div>
+    <div id="main">
+      <div id="navigation">
+        &nbsp;
+      </div>
+      <div id="page">
+        <h2>ADMIN</h2>
+        <p>This is the admin area.</p>
+        <ul>
+          <li><a href="manage_content.php">Manage Content</a></li>
+          <li><a href="manage_admins.php">Manage Users</a></li>
+          <li><a href="logout.php">Logout</a></li>
+        </ul>
+      </div>
+    </div>
+    </body>
 </html>
